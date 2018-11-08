1987f19dcd07ef2b5227636b27e01b689226677e
Ryan Miranda <rpmir1@gmail.com>
2018-11-09 10:21:00 +1100

    Initial commit with project / solution structure.

---
+++ src/Subscribers/SubscriberOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SubscriberOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
